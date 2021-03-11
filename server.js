2d066d35e94fee86833a3a1d1c713a68850182e0
unknown <vishalpawar048@gmail.com>
2021-03-11 09:18:18 +0530

    https

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-var fs = require('fs');
 const app = express();
 const port = 3001;
 const router = require("./routes");
@@ -7,20 +6,13 @@ var cors = require('cors')
 require("./model/db");
 const bodyParser = require("body-parser");
 
-var http = require('http');
-var https = require('https');
-var privateKey  = fs.readFileSync('ssl/key.pem', 'utf8');
-var certificate = fs.readFileSync('ssl/cert.pem', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
-var httpsServer = https.createServer(credentials, app);
-
 app.use(bodyParser.urlencoded());
 app.use(cors())
 app.use(bodyParser.json());
 app.use("/", router);
 
-httpsServer.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 module.exports = {
-  httpsServer
+  app
 };
