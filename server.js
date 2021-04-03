233fd4b34b2d5bc3ef3a74d060f746e0ba3838f6
Pawar <vishal.a.pawar@accenture.com>
2021-04-03 19:41:48 +0530

    ssl

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const router = require("./routes");
 var cors = require('cors')
 require("./model/db");
 const bodyParser = require("body-parser");
-var https = require('https');
-var privateKey  = fs.readFileSync('ssl/bruhh.in.key', 'utf8');
-var certificate = fs.readFileSync('ssl/bruhh.in.crt', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
-var httpsServer = https.createServer(credentials, app);
+// var https = require('https');
+// var privateKey  = fs.readFileSync('ssl/bruhh.in.key', 'utf8');
+// var certificate = fs.readFileSync('ssl/bruhh.in.crt', 'utf8');
+// var credentials = {key: privateKey, cert: certificate};
+// var httpsServer = https.createServer(credentials, app);
 
 app.use(bodyParser.urlencoded());
 app.use(cors())
@@ -18,13 +18,13 @@ app.use(bodyParser.json());
 app.use("/", router);
 
 //Uncommente this line for production
-httpsServer.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// httpsServer.listen(port, () => console.log(`Example app listening on port ${port}!`));
 //Uncomment this for local
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`)
-// })
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
 
 module.exports = {
-  httpsServer
+  app
 };
